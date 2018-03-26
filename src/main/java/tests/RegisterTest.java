6a2ef29d847edbe3f6e63e3e39d724289d019b21
adyd94 <ady.drozman2007@gmail.com>
2018-03-26 18:06:42 +0300

    register v1

---
+++ src/main/java/tests/RegisterTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.LoginModel;
+import models.RegisterModel;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.RegisterPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by casab on 11/2/2017.
+ */
+public class RegisterTest extends BaseTestLogin {
+
+    @DataProvider(name = "RegisterJson")
+    public Iterator<Object[]> jsonLoginDataProvider() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Collection<Object[]> dp = new ArrayList<>();
+
+        File[] files = getListOfFiles("register");
+        for (File f : files) {
+            RegisterModel m = objectMapper.readValue(f, RegisterModel.class);
+            dp.add(new Object[]{m});
+        }
+        return dp.iterator();
+    }
+
+    @Test(dataProvider = "RegisterJson",priority = 1)
+    public void mainRegisterTest(RegisterModel registerModel) throws InterruptedException {
+        RegisterPage registerPage = PageFactory.initElements(driver, RegisterPage.class);
+
+        registerPage.RegisterFlow(registerModel);
+
+    }
+
+
+
+
+
+
+
+
+
+}
